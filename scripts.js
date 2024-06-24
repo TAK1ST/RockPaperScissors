ae5517155d8443665c4b8fb113cd3cd7aeb50513
TAK1ST <90909909+TAK1ST@users.noreply.github.com>
2024-06-24 07:55:35 +0700

    create rule of game and effect in game with javascript

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
+let score = 0;
 
-const button = document.getElementById('Rule');
+function playGame(userChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    if ((userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')) {
+        score++;
+    }
+    if ((userChoice === 'paper' && computerChoice === 'scissors') ||
+    (userChoice === 'scissors' && computerChoice === 'rock') ||
+    (userChoice === 'rock' && computerChoice === 'paper')) {
+    score--;
+}
+    document.getElementById('score').textContent = score;
+}
 
-button.addEventListener('click', function () {
-  let currentOpacity = parseFloat(getComputedStyle(button).getPropertyValue('opacity'));
-  let newOpacity = (currentOpacity < 1) ? currentOpacity + 0.1 : 1;
-  button.style.opacity = newOpacity;
-});
+function showRules() {
+    document.getElementById('rulesModal').style.display = 'flex';
+}
 
+function hideRules() {
+    document.getElementById('rulesModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('rulesModal')) {
+        hideRules();
+    }
+}
